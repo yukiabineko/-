df66af23cbcea9b0922217b15f7f8ac124910537
abi <yukiuematsu@icloud.com>
2023-07-08 07:22:22 +0900

    first

---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>@yield('title')</title>
+  <link rel="stylesheet" href="{{ asset('main.css')}}">
+</head>
+<body>
+  <main>
+    @yield('context')
+  </main>
+</body>
+</html>
