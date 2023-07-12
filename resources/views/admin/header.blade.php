6b8c7c8114c8754c2e21b8d245a653b6e64acaec
abi <yukiuematsu@icloud.com>
2023-07-12 17:19:22 +0900

    管理者用ヘッダースライダー作成まで

--- resources/views/admin/header.blade.php
+++ resources/views/admin/header.blade.php
@@ -2,18 +2,30 @@
   <!-- ロゴ -->
   <div class="icons">
     <a href="#">
-      <img src="{{ asset('image/icon.png')}}" alt="ロゴ">
+      <img src="{{ asset('image/icon3.png')}}" alt="ロゴ">
     </a>
   </div>
   <!-- メニュー -->
   <div class="header-menu">
-    <div class="header-menu-wrapper">
+    <label class="header-menu-wrapper" for="check">
       <div class="admin-user">
         <img src="{{ asset('storage/users'.Auth::user()->id.'/'.Auth::user()->path)}}" alt="アドミンユーザー">
-        <span class="admin-user-name">管理者メニュー</span>
+        <span class="admin-user-name">Menu</span>
       </div>
+    </label>
+    <input type="checkbox" id="check">
+    <span class="header-up">🔺</span>
+    <span class="header-down">🔻</span>
+
+    <!-- スライダーメニュー -->
+    <div class="header-slider">
+      <ul>
+        <li>a</li>
+        <li>b</li>
+        <li>c</li>
+        <li>d</li>
+      </ul>
     </div>
-   
   </div>
 
 </header>
