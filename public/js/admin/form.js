e95bbd72a3401e82657348dae423f7d0c7148695
abi <yukiuematsu@icloud.com>
2023-07-13 10:59:25 +0900

    管理者商品登録ページファイルアップ作成中

--- public/js/admin/form.js
+++ public/js/admin/form.js
@@ -1,3 +1,9 @@
 window.addEventListener('load', ()=>{
-  //alert('test');
+  let file = document.getElementById('file');
+  let fileText = document.querySelector('.file-text');
+
+  file.addEventListener('change', event=>{
+    const name = event.currentTarget.files[0].name;
+    fileText.textContent = name;
+  });
 })
--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -17,66 +17,70 @@
    <article class="page-contents">
      <form action="#" class="product-form" enctype="multipart/form-data" method="POST">
         @csrf
+        <div class="form-wrapper">
 <!--------------------------------------------------------------------------->
-        <!-- 左エリア -->
-        <section class="image-forms">
-           <h4>商品画像登録</h4>
+            <!-- 左エリア -->
+            <section class="image-forms">
+                <h4>商品画像登録</h4>
 
-           <!-- 追加、削除ボタン -->
-           <div class="btns">
-            <button type="button" class="plus">+</button>
-            <button type="button" class="minus">-</button>
-           </div>
+                <!-- 追加、削除ボタン -->
+                <div class="btns">
+                    <button type="button" class="plus">+</button>
+                    <button type="button" class="minus">-</button>
+                </div>
 
-           <!-- ファイル -->
-           <div class="files">
-             <input type="file" name="file[]" class="file" id="file">
-           </div>
-
-        </section>
-
+                <!-- ファイル -->
+                <div class="files">
+                    <div class="file-box">
+                        <input type="file" name="file[]" class="file" id="file">
+                        <span class="file-text"></span>
+                        <label for="file" class="file-label">写真選択</label>
+                    </div>
+                </div>
+            </section>
 <!--------------------------------------------------------------------------->
-         <!-- 右エリア -->
-         <section class="product-forms">
-            <!-- 商品名 -->
-            <div class="form-group">
-                <div class="form-title">商品名</div>
-                <input type="text" name="name" class="form-control" value="{{ old('name', $product->name)}}">
-            </div>
+            <!-- 右エリア -->
+            <section class="product-forms">
+                <h4>商品情報入力</h4>
+                <!-- 商品名 -->
+                <div class="form-group">
+                    <div class="form-title">商品名<span class="status">必須</span></div>
+                    <input type="text" name="name" class="form-control" value="{{ old('name', $product->name)}}">
+                </div>
 
-             <!-- 価格 -->
-             <div class="form-group">
-                <div class="form-title">商品価格</div>
-                <input type="number" min=1  name="price" class="form-control" value="{{ old('price', $product->price)}}">
-             </div>
+                <!-- 価格 -->
+                <div class="form-group">
+                    <div class="form-title">商品価格<span class="status">必須</span></div>
+                    <input type="number" min=1  name="price" class="form-control" value="{{ old('price', $product->price)}}">
+                </div>
 
-            <!-- 在庫 -->
-            <div class="form-group">
-                <div class="form-title">商品在庫</div>
-                <input type="number" min=0  name="stock" class="form-control" value="{{ old('stock', $product->stock)}}">
-            </div>
+                <!-- 在庫 -->
+                <div class="form-group">
+                    <div class="form-title">商品在庫<span class="status">任意</span></div>
+                    <input type="number" min=0  name="stock" class="form-control" value="{{ old('stock', $product->stock)}}">
+                </div>
 
-            <!-- カテゴリー -->
-            <div class="form-group">
-                <div class="form-title">商品カテゴリー</div>
-                <select name="category" class="form-control">
-                    <option value="0" {{ old('title',$product->category) == 0 ? 'selected' : ''}}>日替わり商品</option>
-                    <option value="1" {{ old('title',$product->category) == 1 ? 'selected' : ''}}>干物</option>
-                    <option value="2" {{ old('title',$product->category) == 2 ? 'selected' : ''}}>うなぎ</option>
-                </select>
-            </div>
+                <!-- カテゴリー -->
+                <div class="form-group">
+                    <div class="form-title">商品カテゴリー<span class="status">必須</span></div>
+                    <select name="category" class="form-control">
+                        <option value="0" {{ old('title',$product->category) == 0 ? 'selected' : ''}}>日替わり商品</option>
+                        <option value="1" {{ old('title',$product->category) == 1 ? 'selected' : ''}}>干物</option>
+                        <option value="2" {{ old('title',$product->category) == 2 ? 'selected' : ''}}>うなぎ</option>
+                    </select>
+                </div>
 
-             <!-- 商品説明ー -->
-             <div class="form-group">
-                <div class="form-title">商品説明</div>
-                <textarea name="info"  cols="30" rows="10" class="form-control textare">{{ old('info',$product->info)}}</textarea>
-            </div>
-
-            <!-- ボタン -->
-            <div class="form-btns">
-                <button type="submit">登録</button>
-            </div>
-         </section>
+                <!-- 商品説明ー -->
+                <div class="form-group">
+                    <div class="form-title">商品説明<span class="status">任意</span></div>
+                    <textarea name="info"  cols="30" rows="10" class="form-control textarea">{{ old('info',$product->info)}}</textarea>
+                </div>
+            </section>
+        </div>
+        <!-- ボタン -->
+        <div class="form-btns">
+            <button type="submit">登録</button>
+        </div>
      </form>
    </article>
 
