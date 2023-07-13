aa3b7beb797a91a7fa647c769be937b44a7e525c
msk-ya <masakiabineko@gmail.com>
2023-07-13 11:45:25 +0900

    管理者商品登録継続1

--- public/js/admin/form.js
+++ public/js/admin/form.js
@@ -1,9 +1,54 @@
 window.addEventListener('load', ()=>{
-  let file = document.getElementById('file');
+ /* let file = document.getElementById('file');
   let fileText = document.querySelector('.file-text');
 
   file.addEventListener('change', event=>{
     const name = event.currentTarget.files[0].name;
     fileText.textContent = name;
+
+    let parent = document.querySelector('.files');
+    const before = document.querySelector('.file-box');
+
+
+    let img = document.getElementById('img');
+    if(img){
+      parent.removeChild(img);
+    }
+
+    img = document.createElement('img');
+    img.id = "img";
+    img.style.width = "100%";
+    img.style.aspectRatio = "2 / 1";
+  
+    let reader = new FileReader();
+    reader.onload = ev =>{
+      img.setAttribute('src', ev.target.result);
+    }
+    reader.readAsDataURL(event.target.files[0]);
+    parent.insertBefore(img,before);
+    
   });
+  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
 })
