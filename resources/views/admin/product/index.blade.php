d52ba3936ad3a481dc4ccc2c3570cda30ab29853
msk-ya <masakiabineko@gmail.com>
2023-07-14 21:04:07 +0900

    管理者商品一覧テーブル1

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -18,8 +18,11 @@
 <!------------------------------------------------------------------------------------------------------------------------->
       <!-- 左検索エリア　-->
       <section class="search-area">
-        <h3>商品検索</h3>
-        <p>条件を入力して検索してください</p>
+        <div class="search-title">
+          <h3>商品検索</h3>
+        </div>
+       
+        <p>*条件を入力して検索してください</p>
         <form action="{{ route('admin.products')}}" method="get" class="search-form">
           <!-- 商品名 -->
           <div class="form-group">
