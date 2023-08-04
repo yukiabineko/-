4638ed4379a0645f38eb5dc6515f4067f11f0c71
abi <yukiuematsu@icloud.com>
2023-08-04 20:03:47 +0900

    メール送信処理開始

---
+++ resources/lang/ja/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Your password has been reset!',
+    'sent' => 'We have emailed your password reset link!',
+    'throttled' => '再試行する場合少々お待ちください。',
+    'token' => 'This password reset token is invalid.',
+    'user' => "そのメールアドレスは登録されていません。",
+
+];
--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -9,6 +9,14 @@
 
 @section('contents')
   <div class="container">
+    
+    @error('email')
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+    @enderror
+
+
       <div class="card">
         <div class="card-header">パスワード変更手続き</div>
 
@@ -27,20 +35,13 @@
 
                     <div class="col-md-6">
                         <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                        @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
                     </div>
                 </div>
 
-                <div class="row mb-0">
-                    <div class="col-md-6 offset-md-4">
-                        <button type="submit" class="btn btn-primary">メールアドレスを送信する</button>
-                    </div>
+                <div class="button-form">
+                    <button type="submit" class="btn btn-primary">メールアドレスを送信する</button>
                 </div>
+                
             </form>
         </div>
       </div>
