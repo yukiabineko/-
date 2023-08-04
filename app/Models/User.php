9e88bc77e7636b5a5d8dbc16dc188077f449db72
abi <yukiuematsu@icloud.com>
2023-08-05 06:35:24 +0900

    mail1

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Mail\TestMail;
+use App\Notifications\PasswordResetNotification;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -50,4 +52,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function sendPasswordResetNotification($token)
+    {
+       $this->notify(new PasswordResetNotification($token, new TestMail()));
+    
+    }
+
+
+
 }
