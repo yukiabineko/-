01966a81ed912c3e1e561b5cf73567540e8bdc5f
abi <yukiuematsu@icloud.com>
2023-08-18 19:15:04 +0900

    問い合わせ一覧2

--- public/js/contacts/index.js
+++ public/js/contacts/index.js
@@ -5,7 +5,16 @@ window.addEventListener('load',()=>{
     document.querySelectorAll('.contact-list').forEach( contact =>{
         contact.addEventListener('click', event =>{
             //問い合わせ内容コンテンツ
-            let textContent = contact.children[0].children.length;
+            let textBody= contact.children[0].children[1];
+            if( textBody.dataset.status == 'off' ){
+                textBody.style.display = "flex";
+                textBody.dataset.status = "on";
+
+            }
+            else{
+                textBody.style.display = "none";
+                textBody.dataset.status = "off";
+            }
             
         });
     });
