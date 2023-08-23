f98baf0d0d23e46bc8e28bd64c1ef12787bfb9c8
abi <yukiuematsu@icloud.com>
2023-08-24 06:50:10 +0900

    管理者問い合わせページレイアウト完了

--- public/js/contacts/index.js
+++ public/js/contacts/index.js
@@ -70,6 +70,9 @@ window.addEventListener('load',()=>{
     document.querySelectorAll('.replay').forEach(btn =>{
        btn.addEventListener('click', event=>{
         //押下さらたボタンの位置
+        const buttonY = event.clientY;
+        console.log('ボタン:' +buttonY);
+        console.log('全体:'+ window.innerHeight);
 
 
         const contactId = btn.dataset.contact;
@@ -79,6 +82,10 @@ window.addEventListener('load',()=>{
         fetch(url).then((respose)=>{
            return respose.json();
         }).then((json)=>{
+           let modal =  document.querySelector('.modal');
+           const height = modal.clientHeight;
+           console.log('高さ:' + height)
+
            /*各種取得したデータを格納*/
            document.querySelector('.modal-user-name').textContent = json.name;
            document.querySelector('.modal-contact-title').textContent = json.title;
@@ -87,13 +94,23 @@ window.addEventListener('load',()=>{
 
 
            document.querySelector('.back-ground-layer').classList.add('back-ground-layer-open');
-           document.querySelector('.modal').classList.add('modal-open');
-
+           //modal.classList.add('modal-open');
+           modal.style.top = buttonY + "px";                             //=>モーダルの位置
+           document.documentElement.scrollTop = (buttonY -100) + "px";   //=>スクロールの位置
+           
+       
+           
 
         });
         /**fetch finish */
        });
        /** button push finish */
+    });
+    /**
+     * モーダルのバックレイヤーをクリックした場合モーダル閉じる
+     */
+    document.querySelector('.back-ground-layer').addEventListener('click', ()=>{
+       closeModal();
     });
 });
 /**
@@ -101,5 +118,13 @@ window.addEventListener('load',()=>{
  */
 const closeModal = ()=>{
     document.querySelector('.back-ground-layer').classList.remove('back-ground-layer-open');
-    document.querySelector('.modal').classList.remove('modal-open');
+    document.querySelector('.modal').style.top = - window.innerHeight + "px";
 }
+/*
+const invalidButton = () =>{
+    document.querySelectorAll('.replay').forEach( btn =>{
+        btn.disabled = true;
+    });
+}
+*/
+
