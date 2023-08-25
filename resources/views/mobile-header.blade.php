58629ae2e941a2fa23944e73fc865264db16c43d
abi <yukiuematsu@icloud.com>
2023-08-26 07:10:45 +0900

    モバイルヘッダーレイアウト2

--- resources/views/mobile-header.blade.php
+++ resources/views/mobile-header.blade.php
@@ -1,11 +1,65 @@
 <header class="mobile-header">
   <!-- アイコン(左側) -->
-  <a href="{{ route('home')}}">
+  <a href="{{ route('home')}}" class="mobile-top-icon">
     <img src="{{ asset('image/icon.png')}}" alt="トップアイコン">
   </a>
   <!-- 右側ハンバーガーメニュ -->
   <div class="hamburger-menu">
-    <label for="hamberger-checkbox"></label>
+    <label for="hamberger-checkbox" class="hamburger">
+     
+    </label>
     <input type="checkbox" id="hamberger-checkbox">
+    <span class="hamburger-span"></span>
+    <div class="mobile-menu-background"></div>
+    <!--メニュー -->
+    <div class="menus">
+      <div class="hamberger-title">
+        <h3>メニュー</h3>
+      </div>
+      @if (Auth::check())
+       <div class="mobile-user-info">
+          <img 
+          src="{{ asset('storage/users'.Auth::id().'/'.Auth::user()->path)}}" 
+          alt="user">
+       </div> 
+      @else
+          
+      @endif
+      <!-- 各ページリスト -->
+      <ul class="mobile-page-menus">
+        <li class="mobile-page-list">
+          <a href="{{ route('home')}}" class="nav-list-link">
+            <div class="main-title">TOP</div>
+            <div class="sub-title">トップページ</div>
+          </a>
+        </li>
+        <li class="mobile-page-list">
+          <a href="{{ route('daily.index')}}" class="nav-list-link">
+            <div class="main-title">ARRIVALS</div>
+            <div class="sub-title">生鮮入荷予定</div>
+          </a>
+        </li>
+        <li class="mobile-page-list">
+          <a href="{{ route('products.index') }}" class="nav-list-link">
+            <div class="main-title">ONLINE</div>
+            <div class="sub-title">オンラインショップ</div>
+          </a>
+        </li>
+        <li class="mobile-page-list">
+          <a href="{{ route('shops.index')}}" class="nav-list-link" >
+            <div class="main-title">SHOP</div>
+            <div class="sub-title">お店情報</div>
+          </a>
+        </li>
+        
+        <li class="mobile-page-list">
+          <a href="{{ route('contacts.create')}}" class="nav-list-link">
+            <div class="main-title">CONTACT</div>
+            <div class="sub-title">お問い合わせ</div>
+          </a>
+        </li>
+      </ul>
+
+    </div>
   </div>
 </header>
