cf1534434a824cc774219a3fca7782720789203f
abi <yukiuematsu@icloud.com>
2023-08-28 06:41:04 +0900

    トップページレイアウト

--- resources/views/mobile-header.blade.php
+++ resources/views/mobile-header.blade.php
@@ -14,18 +14,78 @@
 
     <!--メニュー -->
     <div class="menus">
-      <div class="mobile-hamburger-title">
-        <h3>メニュー</h3>
-      </div>
     <!---------- 認証完了時 ------------------------->
       @if (Auth::check())
        <div class="mobile-user-info">
-          <img 
-          src="{{ asset('storage/users'.Auth::id().'/'.Auth::user()->path)}}" 
-          alt="user" class="mobile-auth-img">
-         <!-- お客様名 -->
-         <div class="mobile-user-name">{{ Auth::user()->name}}様</div>
-        <!-- 買い物かご -->
+          <!-- お客様名 -->
+          <div class="mobile-user-name">
+            <img 
+              src="{{ asset('storage/users'.Auth::id().'/'.Auth::user()->path)}}" 
+              alt="user" class="mobile-auth-img">
+            <div class="mobile-name-texxt">{{ Auth::user()->name}}様</div>
+          </div>
+
+          <!-- 買い物かご -->
+          <div class="mobile-cart">
+            <!-- アイコンタイトル  -->
+            <div class="mobile-cart-title">
+              <img src="{{ asset('image/cart1.png')}}" alt="カートかご" class="mobile-cart-icon">
+              <div class="mobile-cart-title">買い物かご</div>
+            </div>
+            <!-- 個数 -->
+            <div 
+             class="mobile-cart-count {{ !empty( session('cart'))? 'mobile-cart-exists' : ''}}"
+             > {{ !empty( session('cart'))? count( session('cart') ) :  0}}</div>
+          </div>
+
+          <!-- ユーザーメニュー -->
+          <ul class="mobile-auth-menu">
+                @if (Auth::user()->admin == 0)
+                <!-- ユーザー情報 -->
+                <li class="menu-li">
+                  <a href="{{ route('users.show', Auth::user() )}}" class="menu-li-link">
+                    <img src="{{ asset('image/users/user.svg')}}" alt="ユーザー" class="menu-link-img">
+                    <span class="menu-link-text">{{ Auth::user()->name}}さん情報</span>
+                  </a>
+                </li>
+
+                <!-- ユーザー編集 -->
+                <li class="menu-li">
+                  <a href="{{ route('users.edit',Auth::user() )}}" class="menu-li-link">
+                    <img src="{{ asset('image/users/edit.svg')}}" alt="編集" class="menu-link-img">
+                    <span class="menu-link-text"> {{ Auth::user()->name}}さん編集</span>
+                  </a>
+                </li>
+
+                <!-- 注文履歴 -->
+                <li class="menu-li">
+                  <a href="{{ route('orders.index')}}" class="menu-li-link">
+                    <img src="{{ asset('image/users/memo.svg')}}" alt="注文履歴" class="menu-link-img">
+                    <span class="menu-link-text">注文履歴</span>
+                  </a>
+                </li>
+              
+                <!-- 注文履歴 -->
+                <li class="menu-li">
+                  <a href="{{ route('contacts.index')}}" class="menu-li-link">
+                    <img src="{{ asset('image/users/contacts.svg')}}" alt="お問い合わせ" class="menu-link-img">
+                    <span class="menu-link-text"> お問い合わせ一覧</span>
+                  </a>
+                </li>
+              @else
+                <li class="menu-li"><a href="{{ route('admin.home')}}" class="menu-li-link">管理画面へ</a></li>  
+              @endif
+              <li class="menu-li">
+                <form action="{{ route('logout')}}" method="post" class="logout-form">
+                  @csrf
+                  <button type="submit" class="logout-button">
+                    <img src="{{ asset('image/users/logout.svg')}}" alt="ログアウト" class="logout-img">
+                    <span class="logout-img-text">ログアウト</span>
+                  </button>
+                </form>
+              </li>
+          </ul>
+
        </div> 
      <!---------- 未認証時 ------------------------->
       @else
